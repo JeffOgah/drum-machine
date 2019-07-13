93595f0527b5ee965ee3bf13822eefcf19b605d5
JeffOgah <jeff.ogah@gmail.com>
2019-07-13 05:48:35 +0100

    Updates

---
+++ precache-manifest.708852b5f2e457d3e10b1b3e7654c45d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d87759b1e656622b82ae11ce455219",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "e7d90718a84a0b99e5e5",
+    "url": "/drum-machine/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "33c0b73b6f307b5c33eb",
+    "url": "/drum-machine/static/css/main.1acaba05.chunk.css"
+  },
+  {
+    "revision": "e7d90718a84a0b99e5e5",
+    "url": "/drum-machine/static/js/2.4a5ba745.chunk.js"
+  },
+  {
+    "revision": "33c0b73b6f307b5c33eb",
+    "url": "/drum-machine/static/js/main.901fc64b.chunk.js"
+  },
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/drum-machine/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/drum-machine/static/media/linkedin.4b475fb3.svg"
+  },
+  {
+    "revision": "c9f1200836515fd4bacd29160be78732",
+    "url": "/drum-machine/static/media/twitter.c9f12008.svg"
+  }
+]);
